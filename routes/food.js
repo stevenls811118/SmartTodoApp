958ff1e334699ba59720c99582753871ebb5b3f2
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2023-01-10 20:28:35 +0000

    commit before merge, do not merge to master

--- routes/food.js
+++ routes/food.js
@@ -1,14 +1,27 @@
-// const express = require('express');
-// const router = express.Router();
-// const {insertItem} = require('../db/queries/books')
+const express = require("express");
+const router = express.Router();
+const { insertItem, getItems, deleteItem } = require("../db/queries/books");
 
+router.post("/", (req, res) => {
+  console.log("req.body: ", req.body);
+  insertItem(req.body.todo_input).then((result) => {
+    console.log(result);
+    res.status(200).send("Ok!");
+  });
+});
 
-// router.post('/', (req, res) => {
-//  insertItem(req.body.food)
-//  .then((result) => {
-//     console.log(result);
-//     res.status(200).send('Ok!');
-//  })
-// })
+router.get("/", (req, res) => {
+  getItems().then((items) => {
+    console.log(items);
+    res.json(items);
+  });
+});
 
-// module.exports = router;
+router.delete("/", (req, res) => {
+  console.log("req.body: ", req.body.deleteBook.trim());
+  deleteItem(req.body.deleteBook.trim()).then((items) => {
+    console.log(items);
+    res.json(items);
+  });
+});
+module.exports = router;
