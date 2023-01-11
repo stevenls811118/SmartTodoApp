727140b339de6dd32ac41fbe1f03216acb81ae38
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2023-01-11 04:46:56 +0000

    transition from rainforest api to serpapi

--- scripts/products_api.js
+++ scripts/products_api.js
@@ -1,22 +1,25 @@
 const axios = require('axios');
+const SerpApi = require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch("secret_api_key");
 
 // set up the request parameters
 const params = {
-api_key: "B7449ED21AF0487C8BF644C4163A80BF",
-  amazon_domain: "amazon.ca",
-  asin: "B073JYC4XM",
-  type: "product"
-}
+  engine: "google_product",
+  product_id: "4172129135583325756",
+  offers: "1",
+  gl: "us",
+  hl: "en"
+};
 
 const productDetails = async(input) => {
+  //set up console.logs for debugging
   console.log("looking for products");
   const productString = input.replace(/ /g, '+').replace(/&/g, '%26');
   console.log(productString);
-  const urlRequest = ('https://api.rainforestapi.com/request');
+  const urlRequest = ('https://serpapi.com/search.json?engine=google_product&product_id=4172129135583325756&offers=1&gl=us&hl=en');
   console.log(urlRequest);
 
-// make the http GET request to Rainforest API
-axios.get('https://api.rainforestapi.com/request', { params })
+  axios.get('https://serpapi.com/search.json?engine=google_product&product_id=4172129135583325756&offers=1&gl=us&hl=en', { params })
 .then(response => {
 
     // print the JSON response from Rainforest API
@@ -26,7 +29,9 @@ axios.get('https://api.rainforestapi.com/request', { params })
 // catch and print the error
 console.log(error);
   })
-}
+};
+
+// Show result as JSON
+search.json(params, productDetails);
 
 module.exports = productDetails;
-
