ad6a8636dcb67c0094b3b3fbfe9750991a0fa79b
Alexander <apvannug.lhl.webdev@gmail.com>
2023-01-10 19:28:46 -0700

    add gbooks api functionality

--- scripts/books-api.js
+++ scripts/books-api.js
@@ -3,47 +3,24 @@ const axios = require('axios');
 const gBooksDetails = async(input) => {
   try{
     // sets API URL request and string variable
-    console.log("beginning book search");
+    // console.log("beginning book search"); //Test Code
     const gBooksString = input.replace(/ /g, '+').replace(/&/g, '%26');
-    console.log(gBooksString);
+    // console.log(gBooksString); //Test Code
     const apiUrlRequest = `https://www.googleapis.com/books/v1/volumes?q=${gBooksString}`;
-    console.log(apiUrlRequest);
+    // console.log(apiUrlRequest); //Test Code
     
     // async promise for Google books API 
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${gBooksString}`);
     // console.log(response.data);
-    console.log(`book has been found!`);
+    // console.log(`book has been found!`); //Test Code
 
     return response.data.kind;
 
-    // .then(res => {
-    //   const bookInfo = res.data.items
-    // })
-
     // catch error message
   } catch (error) {
-    console.log(error.response.body);
-    console.log(`book has not been found!`);
+    console.log(error.response.body); //Test Code
+    // console.log(`book has not been found!`); //Test Code
   }
 };
-
-// const gBooksDetails = async(input) => {
-//   console.log("beginning book search");
-//   const gBooksString = input.replace(/ /g, '+').replace(/&/g, '%26');
-//   const apiUrlRequest = `https://www.googleapis.com/books/v1/volumes?q=${gBooksString}`;
-//   return axios.get(apiUrlRequest).then(res => {
-//     const bookInfo = res.data.items[0].volumeInfo;
-//     console.log("this is a book");
-//     return {
-//       title: bookInfo.title,
-//       subtitle: bookInfo.subtitle,
-//       authors: bookInfo.authors,
-//       year: bookInfo.publishedDate,
-//       categories: bookInfo.categories,
-//       thumbnail: bookInfo.imageLinks.thumbnail,
-//       link: bookInfo.previewLink
-//     }
-//   })
-// };
 
 module.exports = gBooksDetails;
