a22dc721d9bbb5a55e00fd34ae00d6538e4e6c3a
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2023-01-11 00:28:40 +0000

    add debugging console.logs

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
-// const express = require('express');
-// const router = express.Router();
-// const {insertItem} = require('../db/queries/books')
+const express = require("express");
+const router = express.Router();
+const { insertItem, getItems, deleteItem } = require("../db/queries/smart.js");
+const apiFetch = require("../scripts/api");
 
 
 // // router.post('/', (req, res) => {
--- scripts/products_api.js
+++ scripts/products_api.js
@@ -8,6 +8,13 @@ api_key: "B7449ED21AF0487C8BF644C4163A80BF",
   type: "product"
 }
 
+const productDetails = async(input) => {
+  console.log("looking for products");
+  const productString = input.replace(/ /g, '+').replace(/&/g, '%26');
+  console.log(productString);
+  const urlRequest = ('https://api.rainforestapi.com/request');
+  console.log(urlRequest);
+}
 // make the http GET request to Rainforest API
 axios.get('https://api.rainforestapi.com/request', { params })
 .then(response => {
