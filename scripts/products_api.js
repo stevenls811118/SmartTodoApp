a1d886080caa8e8b8064b3fed74f8f0ed5358811
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2023-01-11 19:09:51 +0000

    re-enable products api, add api key to products_api.js

--- scripts/products_api.js
+++ scripts/products_api.js
@@ -1,37 +1,37 @@
-// const axios = require('axios');
-// const SerpApi = require('google-search-results-nodejs');
-// const search = new SerpApi.GoogleSearch("secret_api_key");
+const axios = require('axios');
+const SerpApi = require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch("95b37135ebe388a0274960f4e2d2055aa8ef471b0c9ecd3d468f11386212b91e");
 
-// // set up the request parameters
-// const params = {
-//   engine: "google_product",
-//   product_id: "4172129135583325756",
-//   offers: "1",
-//   gl: "us",
-//   hl: "en"
-// };
+// set up the request parameters
+const params = {
+  engine: "google_product",
+  product_id: "4172129135583325756",
+  offers: "1",
+  gl: "us",
+  hl: "en"
+};
 
-// const productDetails = async(input) => {
-//   //set up console.logs for debugging
-//   console.log("looking for products");
-//   const productString = input.replace(/ /g, '+').replace(/&/g, '%26');
-//   console.log(productString);
-//   const urlRequest = ('https://serpapi.com/search.json?engine=google_product&product_id=4172129135583325756&offers=1&gl=us&hl=en');
-//   console.log(urlRequest);
+const productDetails = async(input) => {
+  //set up console.logs for debugging
+  console.log("looking for products");
+  const productString = input.replace(/ /g, '+').replace(/&/g, '%26');
+  console.log(productString);
+  const urlRequest = ('https://serpapi.com/search.json?engine=google_product&product_id=4172129135583325756&offers=1&gl=us&hl=en');
+  console.log(urlRequest);
 
-//   axios.get('https://serpapi.com/search.json?engine=google_product&product_id=4172129135583325756&offers=1&gl=us&hl=en', { params })
-// .then(response => {
+  axios.get('https://serpapi.com/search.json?engine=google_product&product_id=4172129135583325756&offers=1&gl=us&hl=en', { params })
+.then(response => {
 
-//     // print the JSON response from Rainforest API
-//     console.log(JSON.stringify(response.data, 0, 2));
+    // print the JSON response from serpapi API
+    console.log(JSON.stringify(response.data, 0, 2));
 
-//   }).catch(error => {
-// // catch and print the error
-// console.log(error);
-//   })
-// };
+  }).catch(error => {
+// catch and print the error
+console.log(error);
+  })
+};
 
-// // Show result as JSON
-// search.json(params, productDetails);
+// Show result as JSON
+search.json(params, productDetails);
 
-// module.exports = productDetails;
+module.exports = productDetails;
