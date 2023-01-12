f3aa9f76595ddf62949007f3b4c4da902e049fed
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2023-01-12 23:56:29 +0000

    rename scripts fodler to apis

--- routes/items.js
+++ routes/items.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { insertItem, getItems, deleteItem } = require("../db/queries/smart.js");
 // const apiFetch = require("../scripts/api");
-const gBooksDetails = require("../scripts/books-api.js");
-const productDetails = require("../scripts/products_api")
+const gBooksDetails = require("../Apis/books-api.js");
+const productDetails = require("../Apis/products_api")
 
 router.post("/", (req, res) => {
   console.log("req.body: ", req.body);
