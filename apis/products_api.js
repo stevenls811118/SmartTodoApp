d1010af04a2fd451e88e444c8c7b400749fc5c7a
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2023-01-13 00:41:15 +0000

    push to test

--- apis/products_api.js
+++ apis/products_api.js
@@ -2,23 +2,23 @@ const axios = require('axios');
 const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("95b37135ebe388a0274960f4e2d2055aa8ef471b0c9ecd3d468f11386212b91e");
 
+const params = {
+  engine: "walmart",
+  query: ``,
+  api_key: "1bbfd2432a4089c49efd374ba9c0f6371f681bc378cac02934a60ad1dcd28dfe",
+};
 // set up the request parameters
-
-
-
 const productDetails = async(input) => {
+
+const productString = input.replace(/ /g, '+').replace(/&/g, '%26')
+const urlRequest = (`https://serpapi.com/search.json?engine=walmart&query=${productString}`);
+
+
+
   try {
-
-    const params = {
-      engine: "walmart",
-      query: `${productString}`,
-    };
-
   //set up console.logs for debugging
-  console.log("looking for products");
-  const productString = input.replace(/ /g, '+').replace(/&/g, '%26');
+  console.log("looking for products");;
   console.log(productString);
-  const urlRequest = (`https://serpapi.com/search.json?engine=walmart&query=${productString}`);
   console.log(urlRequest);
 
   const resSerpApi = await axios.get(`https://serpapi.com/search.json?engine=walmart&query=${productString}`)
@@ -32,7 +32,6 @@ console.log(error);
   };
 
 }
-
 // Show result as JSON
-// search.json(params, productDetails);
+search.json(params, productDetails);
 module.exports = productDetails;
