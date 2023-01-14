de130450504fdf3be5b4fd61cd0fa9e9c670dee7
Yifan Shi <stevenls1118@msn.com>
2023-01-14 12:11:46 -0700

    add fillter for products_api

--- apis/products_api.js
+++ apis/products_api.js
@@ -2,6 +2,8 @@ const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("a5da1b8a8cf1954f193059de03c1785017afb51eb30464e96a8c4a35547587e5");
 const serpApiFetch = async(input, req, res) => {
 
+  let inputArray = input.split(' ');
+  console.log(inputArray);
   return new Promise((resolve) => {
 
     const params = {
@@ -16,7 +18,25 @@ const serpApiFetch = async(input, req, res) => {
     };
 
     const callback = function(data) {
-      resolve(data.error);
+      let found = 0;
+      if (data.error) {
+        resolve(data.error);
+      } else {
+        // console.log("data.shopping_results[0] is: ", data.shopping_results[0]);
+        for (let i of inputArray) {
+          if (data.shopping_results[0].title.toLowerCase().includes(i.toLowerCase())) {
+            found++;
+          }
+        }
+        if (found >= 2 / 3 * inputArray.length) {
+          console.log("found is : ", found);
+          console.log("2 / 3 inputArray.length is: ", 2 / 3 * inputArray.length);
+          resolve(undefined);
+        } else {
+          resolve("Product not found");
+        }
+      }
+      
     };
 
     search.json(params, callback);
