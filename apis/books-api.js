f54c68e91fa0f211a954f3bcc194b1990fa2e668
Alexander <apvannug.lhl.webdev@gmail.com>
2023-01-13 20:35:13 -0700

    refactor book api to use OpenLibrary

--- apis/books-api.js
+++ apis/books-api.js
@@ -4,19 +4,23 @@ const gBooksDetails = async(input) => {
   try {
     // sets API URL request and string variable
     // console.log("beginning book search"); //Test Code
-    
+    let openLibString = input.split(' ').join('+');
     // console.log(gBooksString); //Test Code
     
     // async promise for Google books API
-    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${input}`);
+    const response = await axios.get(`http://openlibrary.org/search.json?q=${openLibString}`);
 
-    console.log('Google books data.totalItems: ', response.data.totalItems);
-  
-    return response.data.totalItems;
+    // console.log('Google books data.totalItems: ', response.data.totalItems);
+
+    const bookFound = response.data.numFound;
+
+    console.log(bookFound);
+
+    return bookFound;
 
     // catch error message
   } catch (error) {
-    console.log(error.response.body); //Test Code
+    console.log(error); //Test Code
     // console.log(`book has not been found!`); //Test Code
   }
 };
